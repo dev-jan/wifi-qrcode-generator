415900690e3a8c7f7978bb536400fe6bbb63ee1d
Jan Bucher <jan@jan-bucher.ch>
2018-01-07 18:40:38 +0100

    Add gradle and init basic project settings

---
+++ src/test/java/com/devjan/wifiqrcodegenerator/MainTest.java
@@ -0,0 +1,18 @@
+package com.devjan.wifiqrcodegenerator;
+
+import org.junit.Test;
+
+public class MainTest {
+	@Test
+	public void test_main() {
+		// Arrange
+		String[] args = new String[1];
+		
+		// Act
+		Main.main(args);
+		
+		// Assert
+		// -- nothing to assert at the moment...
+	}
+
+}
