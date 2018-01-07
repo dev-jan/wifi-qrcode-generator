f5117fd6bbfc356afe790ee1207bd3abedabb5c0
Jan Bucher <jan@jan-bucher.ch>
2018-01-07 19:16:34 +0100

    :sparkles: Code beautification

--- src/main/java/com/devjan/wifiqrcodegenerator/Main.java
+++ src/main/java/com/devjan/wifiqrcodegenerator/Main.java
@@ -1,27 +1,38 @@
 package com.devjan.wifiqrcodegenerator;
 
 import java.util.concurrent.Callable;
-
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+/**
+ * Main starter class called by the CLI.
+ *
+ * @author Jan Bucher
+ */
+@Command(name = "wifi-qrcode-generator",
+         header = "Simple cli tool to create a QR code to connect to your wifi.")
 public class Main implements Callable<Void> {
-	@Option(names = { "-v", "--verbose" }, description = "More verbose output")
-	private boolean verbose = false;
+  @Option(names = {"-v", "--verbose"}, description = "More verbose output")
+  private boolean verbose = false;
 
-	@Parameters(index = "0", paramLabel = "OUTPUT-FILE", description = "Output file")
-	private String outputFile;
+  @Parameters(index = "0", paramLabel = "OUTPUT-FILE", description = "Output file")
+  private String outputFile;
 
-	public static void main(String[] args) {
-		// use picocli to handle CLI args and stuff
-		CommandLine.call(new Main(), System.err, args);
-	}
+  /**
+   * Entry function
+   * @param args Command line arguments
+   */
+  public static void main(String[] args) {
+    // use picocli to handle CLI args and stuff
+    CommandLine.call(new Main(), System.err, args);
+  }
 
-	@Override
-	public Void call() throws Exception {
-		System.out.println("Hello world :)");
-		return null;
-	}
+  @Override
+  public Void call() throws Exception {
+    System.out.println("Hello world :)");
+    return null;
+  }
 
 }
--- src/test/java/com/devjan/wifiqrcodegenerator/MainTest.java
+++ src/test/java/com/devjan/wifiqrcodegenerator/MainTest.java
@@ -2,17 +2,22 @@ package com.devjan.wifiqrcodegenerator;
 
 import org.junit.Test;
 
+/**
+ * JUnit test for the class {@link Main}
+ *
+ * @author Jan Bucher
+ */
 public class MainTest {
-	@Test
-	public void test_main() {
-		// Arrange
-		String[] args = new String[0];
-		
-		// Act
-		Main.main(args);
-		
-		// Assert
-		// -- nothing to assert at the moment...
-	}
+  @Test
+  public void test_main() {
+    // Arrange
+    String[] args = new String[0];
+
+    // Act
+    Main.main(args);
+
+    // Assert
+    // -- nothing to assert at the moment...
+  }
 
 }
